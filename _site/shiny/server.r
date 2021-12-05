67a66ed82a81ab42f0664746cfd0a778819539dc
bx-lr <41136019+bx-lr@users.noreply.github.com>
2021-12-04 21:00:29 -0600

    bugfix

--- _site/shiny/server.r
+++
@@ -1,9 +0,0 @@
-server <- function(input, output){
-  output$distPlot <- renderPlot({
-    x <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins +1)
-    hist(x, breaks = bins, col= "#75AADB", border = "white",
-         xlab = "waiting time till next (in min)",
-         main = "histogram of waiting times")
-  })
-}
